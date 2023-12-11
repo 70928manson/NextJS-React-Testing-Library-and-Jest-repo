69507b4156e2c7e72ac48fd10b2e37bed71735b5
Manson <manson972@yahoo.com>
2023-12-11 22:11:07 +0800

    add muti test

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,10 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import Home from '@/app/page';
 
-it('should have Docs test', () => {
-    render(<Home />) //Arrange
+describe('Home', () => {
+    it('should have Docs text', () => {
+        render(<Home />) //Arrange
 
-    const myElement = screen.getByText('Docs') //Act
+        const myElement = screen.getByText('Docs') //Act
 
-    expect(myElement).toBeInTheDocument();
+        expect(myElement).toBeInTheDocument();
+    });
+
+    it('should contain the text "information"', () => {
+        render(<Home />) //Arrange
+
+        const myElement = screen.getByText(/information/i) //Act  //case insensitive
+
+        expect(myElement).toBeInTheDocument();
+    });
+
+    it('should have a heading', () => {
+        render(<Home />) //Arrange
+
+        const myElement = screen.getByRole('heading', {
+            name: 'Learn'
+        }) //Act  //case insensitive
+
+        expect(myElement).toBeInTheDocument();
+    });
 })
