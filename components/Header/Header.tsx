a52e974b749cd650aac48f1dc02c52ab720eaff6
Manson <manson972@yahoo.com>
2023-12-16 21:45:42 +0800

    change interface to type (typescript, not interface script)

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,9 @@
 
-interface IHeader {
+type THeader = {
     title: string
 }
 
-export default function Header({ title }: IHeader) {
+export default function Header({ title }: THeader) {
     return (
         <h1 className="text-3xl font-bold mb-0 text-white/90">
             {title}
