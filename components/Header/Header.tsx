913f687492b9090e28488ffa6873fa5881c9d273
Manson <manson972@yahoo.com>
2023-12-16 19:16:15 +0800

    update Header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,10 +5,8 @@ interface IHeader {
 
 export default function Header({ title }: IHeader) {
     return (
-        <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
-            <div className="max-w-xl mx-auto sm:px-4 flex justify-between">
-                Header
-            </div>
-        </nav>
+        <h1 className="text-3xl font-bold mb-0 text-white/90">
+            {title}
+        </h1>
     )
 }
