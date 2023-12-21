4e4607b8abf31d09c1596f1659a8e870b0a9d92f
Manson <manson972@yahoo.com>
2023-12-21 21:13:35 +0800

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import TodoList from "@/components/TodoList/TodoList"
-import AddTodo from "@/components/AddTodo/AddTodo"
-import { useState } from "react"
-import type { TTodo } from "./types/Todo"
+import TodoList from "@/components/TodoList/TodoList";
+import AddTodo from "@/components/AddTodo/AddTodo";
+import { useState } from "react";
+import type { TTodo } from "./types/Todo";
 
 
 export default function Home() {
@@ -32,7 +32,7 @@ export default function Home() {
       "completed": false,
       "id": 4,
     }
-  ])
+  ]);
 
   return (
     <>
